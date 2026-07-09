71240543e8b7862ff547963b8371eb9973f399ed
Thomas Phong <ino.thomas@outlook.com>
2026-07-09 17:34:18 +0700

    build: create entities: instructors, courses, student_enrollments, and students
    - Session04: Exercise01-02

---
+++ src/main/java/com/example/course_management/entity/StudentEnrollment.java
@@ -0,0 +1,32 @@
+package com.example.course_management.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "student_enrollments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StudentEnrollment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id", nullable = false)
+    private Student student;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+}
