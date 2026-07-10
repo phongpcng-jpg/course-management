6319d76c30261400f60983b246fd8e7e8b457dce
Thomas Phong <ino.thomas@outlook.com>
2026-07-10 10:29:05 +0700

    chore: create copies of instructor, course, enrollment, and instructor detail services with profile old in legacy package
    - Session04: Exercise03

---
+++ src/main/java/com/example/course_management/service/impl/legacy/CourseServiceImpl.java
@@ -0,0 +1,98 @@
+package com.example.course_management.service.impl.legacy;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import com.example.course_management.model.Course;
+import com.example.course_management.repository.CourseRepository;
+import com.example.course_management.service.ICourseService;
+import com.example.course_management.service.IInstructorService;
+
+
+@Service
+@Profile("old")
+public class CourseServiceImpl implements ICourseService{
+
+    private final CourseRepository courseRepository;
+    private final IInstructorService instructorService;
+
+    @Autowired
+    public CourseServiceImpl(
+        CourseRepository courseRepository,
+        IInstructorService instructorService
+    ) {
+
+        this.courseRepository = courseRepository;
+
+        this.instructorService = instructorService;
+
+    }
+
+    @Override
+    public List<Course> getAllCourses() {
+        return courseRepository.findAll();
+    }
+
+    @Override
+    public Course getCourseById(Long id) {
+        return courseRepository.findById(id)
+                .orElseThrow(
+                    () -> new RuntimeException(
+                        "Course not found."
+                    )
+                );
+    }
+
+    @Override
+    public Course createCourse(Course course) {
+
+        try {
+            instructorService.getInstructorById(
+                course.getInstructorId()
+            );
+        } catch (RuntimeException e) {
+            throw new RuntimeException(
+                "Instructor of course not found."
+            );
+        }
+
+        return courseRepository.create(course);
+        
+    }
+
+    @Override
+    public Course updateCourse(Long id, Course course) {
+        
+        try {
+            instructorService.getInstructorById(
+                course.getInstructorId()
+            );
+        } catch (RuntimeException e) {
+            throw new RuntimeException(
+                "Instructor of course not found."
+            );
+        }
+
+        return courseRepository.update(id, course)
+                .orElseThrow(
+                    () -> new RuntimeException(
+                        "Course not found."
+                    )
+                );
+
+    }
+
+    @Override
+    public Course deleteCourseById(Long id) {
+        return courseRepository.deleteById(id)
+                .orElseThrow(
+                    () -> new RuntimeException(
+                        "Course not found."
+                    )
+                );
+    }
+
+}
---
+++ src/main/java/com/example/course_management/service/impl/legacy/EnrollmentServiceImpl.java
@@ -0,0 +1,247 @@
+package com.example.course_management.service.impl.legacy;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import com.example.course_management.dto.enrollment.EnrollCourseRequest;
+import com.example.course_management.dto.enrollment.EnrollmentDetail;
+import com.example.course_management.enums.CourseStatus;
+import com.example.course_management.model.Course;
+import com.example.course_management.model.Enrollment;
+import com.example.course_management.repository.EnrollmentRepository;
+import com.example.course_management.service.ICourseService;
+import com.example.course_management.service.IEnrollmentService;
+import com.example.course_management.service.IInstructorService;
+
+
+@Service
+@Profile("old")
+public class EnrollmentServiceImpl implements IEnrollmentService{
+
+    private final EnrollmentRepository enrollmentRepository;
+    private final ICourseService courseService;
+    private final IInstructorService instructorService;
+
+    @Autowired
+    public EnrollmentServiceImpl(
+        EnrollmentRepository enrollmentRepository,
+        ICourseService courseService,
+        IInstructorService instructorService
+    ) {
+
+        this.enrollmentRepository = enrollmentRepository;
+        this.courseService = courseService;
+        this.instructorService = instructorService;
+
+    }
+
+    @Override
+    public List<Enrollment> getAllEnrollment() {
+        return enrollmentRepository.findAll();
+    }
+
+    @Override
+    public Enrollment getEnrollmentById(Long id) {
+        return enrollmentRepository.findById(id)
+                .orElseThrow(
+                    () -> new RuntimeException(
+                        "Enrollment not found."
+                    )
+                );
+    }
+
+    @Override
+    public Enrollment createEnrollmentOld(Enrollment enrollment) {
+
+        Course course;
+
+        try {
+            course = courseService.getCourseById(enrollment.getCourseId());
+        } catch (RuntimeException e) {
+            throw new RuntimeException(
+                "Course of enrollment not found."
+            );
+        }
+
+        if (course.getStatus() != CourseStatus.ACTIVE) {
+            throw new RuntimeException(
+                "Course is not active."
+            );
+        }
+        
+        try {
+            instructorService.getInstructorById(
+                course.getInstructorId()
+            );
+        } catch (RuntimeException e) {
+            throw new RuntimeException(
+                "Instructor of enrollment's course not found."
+            );
+        }
+
+        return enrollmentRepository.create(enrollment);
+
+    }
+
+    @Override
+    public EnrollmentDetail createEnrollment(
+        EnrollCourseRequest enrollmentRequest) {
+
+        Enrollment enrollment = Enrollment.builder()
+                    .studentName(enrollmentRequest.getStudentName())
+                    .courseId(enrollmentRequest.getCourseId())
+                    .build();
+
+        Course course;
+
+        try {
+            course = courseService.getCourseById(enrollment.getCourseId());
+        } catch (RuntimeException e) {
+            throw new RuntimeException(
+                "Course of enrollment not found."
+            );
+        }
+
+        if (course.getStatus() != CourseStatus.ACTIVE) {
+            throw new RuntimeException(
+                "Course is not active."
+            );
+        }
+        
+        try {
+            instructorService.getInstructorById(
+                course.getInstructorId()
+            );
+        } catch (RuntimeException e) {
+            throw new RuntimeException(
+                "Instructor of enrollment's course not found."
+            );
+        }
+
+        Enrollment createdEnrollment =  enrollmentRepository.create(enrollment);
+
+        try {
+            return EnrollmentDetail.builder()
+                    .id(createdEnrollment.getId())
+                    .studentName(createdEnrollment.getStudentName())
+                    .course(courseService.getCourseById(
+                        createdEnrollment.getCourseId()
+                    )).build();
+        } catch (RuntimeException e) {
+            throw new RuntimeException(
+                "Course of enrollment not found after created."
+            );
+        }
+
+    }
+
+    @Override
+    public Enrollment updateEnrollmentOld(Long id, Enrollment enrollment) {
+
+        Course course;
+
+        try {
+            course = courseService.getCourseById(enrollment.getCourseId());
+        } catch (RuntimeException e) {
+            throw new RuntimeException(
+                "Course of enrollment not found."
+            );
+        }
+
+        if (course.getStatus() != CourseStatus.ACTIVE) {
+            throw new RuntimeException(
+                "Course is not active."
+            );
+        }
+        
+        try {
+            instructorService.getInstructorById(
+                course.getInstructorId()
+            );
+        } catch (RuntimeException e) {
+            throw new RuntimeException(
+                "Instructor of enrollment's course not found."
+            );
+        }
+
+        return enrollmentRepository.update(id, enrollment)
+                .orElseThrow(
+                    () -> new RuntimeException(
+                        "Enrollment not found."
+                    )
+                );
+
+    }
+
+    @Override
+    public EnrollmentDetail updateEnrollment(
+        Long id, 
+        EnrollCourseRequest enrollmentRequest) {
+
+        Enrollment enrollment = Enrollment.builder()
+                    .studentName(enrollmentRequest.getStudentName())
+                    .courseId(enrollmentRequest.getCourseId())
+                    .build();
+
+        Course course;
+
+        try {
+            course = courseService.getCourseById(enrollment.getCourseId());
+        } catch (RuntimeException e) {
+            throw new RuntimeException(
+                "Course of enrollment not found."
+            );
+        }
+
+        if (course.getStatus() != CourseStatus.ACTIVE) {
+            throw new RuntimeException(
+                "Course is not active."
+            );
+        }
+        
+        try {
+            instructorService.getInstructorById(
+                course.getInstructorId()
+            );
+        } catch (RuntimeException e) {
+            throw new RuntimeException(
+                "Instructor of enrollment's course not found."
+            );
+        }
+
+        Enrollment updatedEnrollment =  enrollmentRepository.update(id, enrollment)
+                .orElseThrow(
+                    () -> new RuntimeException(
+                        "Enrollment not found."
+                    )
+                );
+        
+        try {
+            return EnrollmentDetail.builder()
+                    .id(updatedEnrollment.getId())
+                    .studentName(updatedEnrollment.getStudentName())
+                    .course(courseService.getCourseById(
+                        updatedEnrollment.getCourseId()
+                    )).build();
+        } catch (RuntimeException e) {
+            throw new RuntimeException(
+                "Course of enrollment not found after updated."
+            );
+        }
+
+    }
+
+    @Override
+    public Enrollment deleteEnrollmentById(Long id) {
+        return enrollmentRepository.deleteById(id)
+                .orElseThrow(
+                    () -> new RuntimeException(
+                        "Enrollment not found."
+                    )
+                );
+    }
+
+}
