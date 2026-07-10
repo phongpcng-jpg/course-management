123ffd94495ef041d2aaaffe2f232aa0d3d01ba5
Thomas Phong <ino.thomas@outlook.com>
2026-07-10 16:05:26 +0700

    build: create student_enrollment request and response dto
    - Session04: Exercise04

---
+++ src/main/java/com/example/course_management/dto/student_enrollment/StudentEnrollmentResponse.java
@@ -0,0 +1,24 @@
+package com.example.course_management.dto.student_enrollment;
+
+import com.example.course_management.dto.course.CourseResponse;
+import com.example.course_management.dto.student.StudentResponse;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StudentEnrollmentResponse {
+
+    private StudentResponse student;
+
+    private CourseResponse course;
+
+}
