ec0f2b6f723feb597e2dc670b4e994ab306e9d2a
Thomas Phong <ino.thomas@outlook.com>
2026-07-13 15:17:16 +0700

    build: update/add find projected by status to course repository
    - Session05: Exercise04

--- src/main/java/com/example/course_management/repository/jpa/CourseRepository.java
+++ src/main/java/com/example/course_management/repository/jpa/CourseRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.example.course_management.dto.course.CourseResponseV2;
 import com.example.course_management.entity.Course;
 import com.example.course_management.enums.CourseStatus;
 
@@ -19,6 +20,20 @@ public interface CourseRepository
         WHERE c.status = :status
     """)
     Page<Course> findAllByStatus(
+        @Param("status") CourseStatus status,
+        Pageable pageable
+    );
+
+    @Query("""
+        SELECT new com.example.course_management.dto.course.CourseResponseV2(
+            c.id,
+            c.title,
+            c.status
+        )
+        FROM Course c
+        WHERE c.status = :status
+    """)
+    Page<CourseResponseV2> findProjectedByStatus(
         @Param("status") CourseStatus status,
         Pageable pageable
     );
