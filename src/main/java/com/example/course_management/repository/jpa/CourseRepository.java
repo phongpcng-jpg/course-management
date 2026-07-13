f1be162f09c66de78fc9ba21b602f0d58252aed7
Thomas Phong <ino.thomas@outlook.com>
2026-07-13 14:56:07 +0700

    build: update/add find all by status to course repository
    - Session05: Exercise03

--- src/main/java/com/example/course_management/repository/jpa/CourseRepository.java
+++ src/main/java/com/example/course_management/repository/jpa/CourseRepository.java
@@ -1,10 +1,26 @@
 package com.example.course_management.repository.jpa;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.course_management.entity.Course;
+import com.example.course_management.enums.CourseStatus;
 
 public interface CourseRepository 
-        extends JpaRepository<Course, Long>{
+        extends JpaRepository<Course, Long>
+{
+
+    @Query("""
+        SELECT c
+        FROM Course c
+        WHERE c.status = :status
+    """)
+    Page<Course> findAllByStatus(
+        @Param("status") CourseStatus status,
+        Pageable pageable
+    );
 
 }
